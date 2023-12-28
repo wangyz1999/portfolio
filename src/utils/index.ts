13b9dedce9fd021e0283e55240f490eec444b4db
Yunzhe Wang <wangyz1999@hotmail.com>
2023-12-27 20:06:01 -0500

    fix bug

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,4 +3,3 @@ export { elementHasClass, toggleClass } from "./domElement";
 export { sortMDByDate, getUniqueTags, getUniqueTagsWithCount } from "./post";
 export { selectBySlug } from "./project";
 export { sortResearchByDate } from "./research";
-export { getImages } from "./images";
