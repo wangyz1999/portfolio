c12949c434e916f48cc66d5e5cdedd4aa15993f2
Yunzhe Wang <wangyz1999@hotmail.com>
2023-12-27 19:49:46 -0500

    update utils

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -4,7 +4,8 @@ import { join } from "path";
 export async function getImages(folder: string, prefix: string) {
     const directoryPath = join(process.cwd(), `public/imgs/${folder}`);
 
-    const files = await fs.readdir(directoryPath);
+    let files = await fs.readdir(directoryPath);
+    files = files.filter(file => file.startsWith(prefix));
     let images = [];
     for (let i = 0; i < files.length; i++) {
         images.push(`/imgs/${folder}/${prefix}${i}.jpeg`);
