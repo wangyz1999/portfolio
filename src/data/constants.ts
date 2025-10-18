4a84c9ae97929c34ac9c73a0b1fbe27505022293
Yunzhe Wang <wangyz1999@hotmail.com>
2025-10-18 16:01:26 -0700

    feat: update research content

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -4,21 +4,29 @@ export const MENU_LINKS = [
 		path: "/",
 	},
 	{
-		title: "Project",
-		path: "/project/",
-	},
-	{
 		title: "Blog",
 		path: "/posts/",
 	},
-	// {
-	// 	title: "Hobbies",
-	// 	path: "/hobbies/",
-	// },
-	// {
-	// 	title: "Snowball",
-	// 	path: "/snowball/",
-	// },
+	{
+		title: "LinkedIn",
+		path: "https://www.linkedin.com/in/yunzhe-wang/",
+		external: true,
+	},
+	{
+		title: "GitHub",
+		path: "https://github.com/wangyz1999",
+		external: true,
+	},
+	{
+		title: "Google Scholar",
+		path: "https://scholar.google.com/citations?user=Z_z6HywAAAAJ",
+		external: true,
+	},
+	{
+		title: "CV",
+		path: "/cv.pdf",
+		external: true,
+	},
 ];
 
 // ! Remember to add your own socials
